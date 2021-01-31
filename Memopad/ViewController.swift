c7621694115fe4f65ea40a507cf15d3ac4c5af62
AnnaGoto <tomato0141pasta@gmail.com>
2021-02-01 01:04:00 +0900

    Initial Commit

---
+++ Memopad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memopad
+//
+//  Created by Mac on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
